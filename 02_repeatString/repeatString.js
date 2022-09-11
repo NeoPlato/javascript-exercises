e2c905d9f52bdb60ea6492ea4a1d565969230a8c
Allan Muruha <allanmuruha@gmail.com>
2022-09-11 20:34:38 +0300

    Complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+  let repeatedString = "";
+  if (number < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < number; i++) {
+    repeatedString += text;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
