6e4f8905b43485412e60790a3dbe34295ea4fb36
Allan Muruha <allanmuruha@gmail.com>
2022-09-11 23:54:28 +0300

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (typeof start != 'number' || typeof end != 'number') {
+      return "ERROR";
+    }
+    if (start < 0 || end < 0) {
+      return "ERROR";
+    }
+    let N = Math.abs(end - start) + 1;
+    return N * (start + end) / 2;
 };
 
 // Do not edit below this line
