c616d3c45b8523651881bab11d1d9d9f67594b03
Allan Muruha <allanmuruha@gmail.com>
2022-09-11 23:40:38 +0300

    Complete removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...arrayItems) {
+  for (let i = 0; i < arrayItems.length; i++) {
+    const item = arrayItems[i];
+    const index = array.indexOf(item);
+    if (index === -1) continue;
+    array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
