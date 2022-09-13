b42c82884b5e51b17ee6cda49c5175402cb6784d
Allan Muruha <allanmuruha@gmail.com>
2022-09-13 17:49:28 +0300

    Complete tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(degreesFahrenheit) {
+  let degreesCelsius = (degreesFahrenheit - 32) *  5 / 9;
+  return Math.round(degreesCelsius * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(degreesCelsius) {
+  let degreesFahrenheit = degreesCelsius * 1.8 + 32;
+  return Math.round(degreesFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
