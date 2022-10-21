695d8cfe4b8f7ceda35be8db5e36bb82c44ba9d0
Allan Muruha <allanmuruha@gmail.com>
2022-10-21 22:25:37 +0300

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[.,! ]/g, "").toLowerCase();
+    reversedString = string.split("").reverse();
+    for (let i=0; i<reversedString.length; i++) {
+        if (string.charAt(i) !== reversedString[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
