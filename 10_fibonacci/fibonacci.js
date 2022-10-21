3aa58d752d6d7d55ce3d09c2d505fe2cfc1aaaf4
Allan Muruha <allanmuruha@gmail.com>
2022-10-21 22:33:30 +0300

    Complete fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(N) {
+    if (N <= 0) return "OOPS";
+    let a = 1;
+    let b = 1;
+    for (let i=2; i<=N; i++) {
+        [a, b] = [b, a+b];
+    }
+    return a;
 };
 
 // Do not edit below this line
