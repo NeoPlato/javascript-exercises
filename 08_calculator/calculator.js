8ee75f07a43e2d334a23aa044d58de4ec50925e7
Allan Muruha <allanmuruha@gmail.com>
2022-10-21 22:05:23 +0300

    Complete calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.length === 0 ? 0 : numbers.reduce((a, b) => a + b);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((a, b) => a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	F = 1;
+  
+  for (let i=1; i<=n; i++) {
+    F *= i;
+  }
+  return F;
 };
 
 // Do not edit below this line
